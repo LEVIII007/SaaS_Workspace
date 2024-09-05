728b45a6b982bee9a3756768982edbe22a2da3bd
“Shashank <shashanktyagiji12345@gmail.com>
2024-09-05 23:01:02 +0530

    chore: Refactor Sidebar component to improve code structure and readability

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,43 +1,121 @@
-import { create } from 'domain';
-import React from 'react'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import React from 'react';
+
 import { cookies } from 'next/headers';
-import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
-import { redirect } from 'next/dist/server/api-utils';
-import { getFolder } from '@/lib/supabase/queries';
+import {
+  getCollaboratingWorkspaces,
+  getFolders,
+  getPrivateWorkspaces,
+  getSharedWorkspaces,
+  getUserSubscriptionStatus,
+} from '@/lib/supabase/queries';
+import { redirect } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+import WorkspaceDropdown from './workspace-dropdown';
+import PlanUsage from './plan-usage';
+import NativeNavigation from './native-navigation';
+import { ScrollArea } from '../ui/scroll-area';
+import FoldersDropdownList from './folders-dropdown-list';
+import UserCard from './user-card';
 
 interface SidebarProps {
-    params : {
-        workspaceId : string
-    };
-    className? : string;
+  params: { workspaceId: string };
+  className?: string;
 }
 
-export const Sidebar: React.FC<SidebarProps> = async ({params, className}) => {
-    const supabase = createServerComponentClient({ cookies });
+const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
+  const supabase = createServerComponentClient({ cookies });
+  //user
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  //subscr
+  const { data: subscriptionData, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  //folders
+  const { data: workspaceFolderData, error: foldersError } = await getFolders(
+    params.workspaceId
+  );
+  //error
+  if (subscriptionError || foldersError) redirect('/dashboard');
+
+  const [privateWorkspaces, collaboratingWorkspaces, sharedWorkspaces] =
+    await Promise.all([
+      getPrivateWorkspaces(user.id),
+      getCollaboratingWorkspaces(user.id),
+      getSharedWorkspaces(user.id),
+    ]);
+
+  //get all the different workspaces private collaborating shared
+  return (
+    <aside
+      className={twMerge(
+        'hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between',
+        className
+      )}
+    >
+      <div>
+        <WorkspaceDropdown
+          privateWorkspaces={privateWorkspaces}
+          sharedWorkspaces={sharedWorkspaces}
+          collaboratingWorkspaces={collaboratingWorkspaces}
+          defaultValue={[
+            ...privateWorkspaces,
+            ...collaboratingWorkspaces,
+            ...sharedWorkspaces,
+          ].find((workspace) => workspace.id === params.workspaceId)}
+        />
+        <PlanUsage
+          foldersLength={workspaceFolderData?.length || 0}
+          subscription={subscriptionData}
+        />
+        <NativeNavigation myWorkspaceId={params.workspaceId} />
+        <ScrollArea
+          className="overflow-scroll relative
+          h-[450px]
+        "
+        >
+          <div
+            className="pointer-events-none 
+          w-full 
+          absolute 
+          bottom-0 
+          h-20 
+          bg-gradient-to-t 
+          from-background 
+          to-transparent 
+          z-40"
+          />
+          <FoldersDropdownList
+            workspaceFolders={workspaceFolderData || []}
+            workspaceId={params.workspaceId}
+          />
+        </ScrollArea>
+      </div>
+      <UserCard subscription={subscriptionData} />
+    </aside>
+  );
+};
+
+export default Sidebar;
+
+
+
+
+
+
+
+
+
+
+
     //FIRST CHECK IF THERE IS A USER
     // THEN CHECK SUBSCRIPTION STATUS
     // CHECK IF FOLDERS EXIST, WE WANT TO SHOW THE FOLDERS IN SIDEBAR
     //CHECK FOR ERRORS, IF YES THEN REDIRECT TO DASHBOARD
     // GET ALL THE DIFFERENT WORKSPACES (PRIVATE AND SHARED)
     // 
-
-    const { data: { user } } = await supabase.auth.getUser();
-
-    if(!user) return;
-
-    const {data : subscriptionData, error : subscriptionError} = await getUserSubscriptionStatus(user.id);
-
-    const {data : workspaceFolderData, erorr : folderError} = await getFolder(params.workspaceId);
-
-    if(subscriptionError || folderError) return redirect('/dashboard');
-
-
-
-
-  return (
-    <div>sidebar</div>
-  )
-}
-
-export default Sidebar
