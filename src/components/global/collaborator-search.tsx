f8156a3b74f2d819d4a110624c1a06ed8e586460
“Shashank <shashanktyagiji12345@gmail.com>
2024-09-06 22:55:09 +0530

    feat: Add debouncing logic for collaborator search

--- src/components/global/collaborator-search.tsx
+++ src/components/global/collaborator-search.tsx
@@ -1,38 +1,135 @@
-import React, { use } from 'react'
-import { User } from '@/lib/supabase/supabase.types';
+'use client';
 import { useSupabaseUser } from '@/lib/providers/supabase-user-provider';
-import { useEffect } from 'react';
+import { User } from '@/lib/supabase/supabase.types';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Label } from '../ui/label';
+import { Search } from 'lucide-react';
+import { Input } from '../ui/input';
+import { ScrollArea } from '../ui/scroll-area';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Button } from '../ui/button';
+import { getUsersFromSearch } from '@/lib/supabase/queries';
 
 interface CollaboratorSearchProps {
-    existingCollaborators: User[]  | [];
-    getCollaborator : (collaborators: User) => void;
-    children : React.ReactNode;
-};
-
-
-
-
-const CollaboratorSearch: React.FC<CollaboratorSearchProps> = ({existingCollaborators, getCollaborator, children}) => {
-    const user = useSupabaseUser();
-    const [search, setSearch] = React.useState<User[] | []>([]);
-    const timeRef = React.useRef<ReturnType<typeof setTimeout>>();
-
-
-    useEffect(() => {
-        if (search.length > 0) {
-            timeRef.current = setTimeout(() => {
-                setSearch([]);
-            }, 3000);
-        }
-        return () => {
-            clearTimeout(timeRef.current);
-        };
-    }, [search]);
-            
-  return (
-    <div>CollaboratorSearch
-    </div>
-  )
+  existingCollaborators: User[] | [];
+  getCollaborator: (collaborator: User) => void;
+  children: React.ReactNode;
 }
 
-export default CollaboratorSearch
+const CollaboratorSearch: React.FC<CollaboratorSearchProps> = ({
+  children,
+  existingCollaborators,
+  getCollaborator,
+}) => {
+  const { user } = useSupabaseUser();
+  const [searchResults, setSearchResults] = useState<User[] | []>([]);
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) clearTimeout(timerRef.current);
+    };
+  }, []);
+
+  const getUserData = ()=>{}
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {                 // debouned search function
+    if (timerRef) clearTimeout(timerRef.current);
+    // timerRef.current = setTimeout(async () => {
+    //   const res = await getUsersFromSearch(e.target.value);
+    //   setSearchResults(res);
+    // }, 450  );
+  };
+
+  const addCollaborator = (user: User) => {
+    getCollaborator(user);
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger className="w-full">{children}</SheetTrigger>
+      <SheetContent className="w-[400px] sm:w-[540px]">
+        <SheetHeader>
+          <SheetTitle>Search Collaborator</SheetTitle>
+          <SheetDescription>
+            <p className="text-sm text-muted-foreground">
+              You can also remove collaborators after adding them from the
+              settings tab.
+            </p>
+          </SheetDescription>
+        </SheetHeader>
+        <div
+          className="flex justify-center
+          items-center
+          gap-2
+          mt-2
+        "
+        >
+          <Search />
+          <Input
+            name="name"
+            className="dark:bg-background"
+            placeholder="Email"
+            onChange={onChangeHandler}
+          />
+        </div>
+        <ScrollArea
+          className="mt-6
+          overflow-y-scroll
+          w-full
+          rounded-md
+        "
+        >
+          {searchResults
+            .filter(
+              (result) =>
+                !existingCollaborators.some(
+                  (existing) => existing.id === result.id
+                )
+            )
+            .filter((result) => result.id !== user?.id)
+            .map((user) => (
+              <div
+                key={user.id}
+                className=" p-4 flex justify-between items-center"
+              >
+                <div className="flex gap-4 items-center">
+                  <Avatar className="w-8 h-8">
+                    <AvatarImage src="/avatars/7.png" />
+                    <AvatarFallback>CP</AvatarFallback>
+                  </Avatar>
+                  <div
+                    className="text-sm 
+                  gap-2 
+                  overflow-hidden 
+                  overflow-ellipsis 
+                  w-[180px] 
+                  text-muted-foreground
+                  "
+                  >
+                    {user.email}
+                  </div>
+                </div>
+                <Button
+                  variant="secondary"
+                  onClick={() => addCollaborator(user)}
+                >
+                  Add
+                </Button>
+              </div>
+            ))}
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CollaboratorSearch;
