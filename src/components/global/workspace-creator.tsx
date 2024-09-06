e8caf232b73f5bc166a9d545d5ff81fce14d451e
“Shashank <shashanktyagiji12345@gmail.com>
2024-09-06 13:52:20 +0530

    Refactor WorkspaceCreator component to improve code structure and readability

--- src/components/global/workspace-creator.tsx
+++ src/components/global/workspace-creator.tsx
@@ -2,9 +2,24 @@
 import {User} from '@/lib/supabase/supabase.types';
 import React, { use, useState } from 'react'
 
+
 export const WorkspaceCreator = () => {
     const [permissions, setPermission] = useState();
+    const [workspace, setWorkspace] = useState('private');
+    const [workspaceName, setWorkspaceName] = useState('');
+    const [collaborators, setCollaborators] = useState<User[]>([]);
+    const addCollaborator = (user : User) => {
+        setCollaborators([...collaborators, user]);
+    };
+
+    const removeCollaborator = (user : User) => {
+        setCollaborators(collaborators.filter((collaborator) => collaborator !== user));
+    };
+
+
   return (
     <div>WorkspaceCreator</div>
   )
+
+
 }
