5092c67f60a482e5a8a9a1b763d80ddba269f871
“Shashank <shashanktyagiji12345@gmail.com>
2024-09-06 12:37:02 +0530

    Refactor custom dialog trigger to use children as a dialog trigger

--- src/components/global/custom-dialog-trigger.tsx
+++ src/components/global/custom-dialog-trigger.tsx
@@ -21,8 +21,28 @@ interface customdialogtriggerProps {
       className? : string;
     }
 
-export const customdialogtrigger = () => {
-  return (
-    <div>customdialogtrigger</div>
+export const customdialogtrigger : React.FC<customdialogtriggerProps> = ({
+    header,
+    content,
+    children,
+    description,
+    footer,
+    className
+}) => {
+
+  return (                                      // so basically we have modieid the dialog trigger to make whole children into A dialog trigger. we will pass chilren into dialog trigger  
+    <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
   )
 }
---
+++ src/components/global/workspace-creator.tsx
@@ -0,0 +1,10 @@
+'use client'; 
+import {User} from '@/lib/supabase/supabase.types';
+import React, { use, useState } from 'react'
+
+export const WorkspaceCreator = () => {
+    const [permissions, setPermission] = useState();
+  return (
+    <div>WorkspaceCreator</div>
+  )
+}
