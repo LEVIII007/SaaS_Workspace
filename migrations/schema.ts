4b04f1111ec9b8525fe42415afdc587590374ca4
“Shashank <shashanktyagiji12345@gmail.com>
2024-09-09 21:02:09 +0530

    Refactor workspaceId reference in collaborators table

--- migrations/schema.ts
+++ migrations/schema.ts
@@ -135,7 +135,7 @@ export const workspace = pgTable("workspace", {
 export const collaborators = pgTable('collaborators', {
 	workspaceId: uuid('workspace_id')
 	  .notNull()
-	  .references(() => workspaces.id, { onDelete: 'cascade' }),
+	  .references(() => workspace.id, { onDelete: 'cascade' }),
 	createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' })
 	  .defaultNow()
 	  .notNull(),
