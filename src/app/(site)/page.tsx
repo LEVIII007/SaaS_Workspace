284f8db46aa8e95a6ac18d4199725ba1b6167425
“Shashank <shashanktyagiji12345@gmail.com>
2024-10-04 14:24:19 +0530

    Refactor Docker Compose file to include production and development services

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
             bg-background
           "
           >
-            Get Cypress Free
+            Get your workspaces Free!
           </Button>
         </div>
         <div
