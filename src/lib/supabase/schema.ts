f631b9ecd10b149b24be57e8290dafd0e2949538
“Shashank <shashanktyagiji12345@gmail.com>
2025-01-08 09:23:59 +0530

    Define users table schema in Supabase; remove commented-out import and add fields for user details

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -8,10 +8,19 @@ import {
   timestamp,
   uuid,
 } from 'drizzle-orm/pg-core';
-import {
-  users,
-} from '../../../migrations/schema';
+// import {
+//   users,
+// } from '../../../migrations/schema';
 
+export const users = pgTable("users", {
+	id: uuid("id").primaryKey().notNull(),
+	fullName: text("full_name"),
+	avatarUrl: text("avatar_url"),
+	billingAddress: jsonb("billing_address"),
+	updatedAt: timestamp("updated_at", { withTimezone: true, mode: 'string' }),
+	paymentMethod: jsonb("payment_method"),
+	email: text("email"),
+});
 
 export const workspaces = pgTable('workspaces', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
